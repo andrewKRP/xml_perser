7442b0a2b4998f0ffe607d2ef39d453b4d715d33
a.karpihin <and.karpikhin@gmail.com>
2021-09-02 11:53:26 +0300

    "With selecting"

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import logging
 
 
 #logger = logging.getLogger(name)
-#fhhh
 #stream_handler = logging.StreamHandler()
 #logger.addHandler(stream_handler)
 
-logging.basicConfig(filename='script_log.log', level=logging.INFO)
+FORMAT = '%(levelname)s %(message)s'
+
+logging.basicConfig(filename='script_log_new.log', level=logging.INFO, format=FORMAT)
 
 i = 0
 path = input("Enter the path to XML file(exp. C:/Users/file.xml): ")
@@ -34,20 +35,23 @@ for elem in root.findall('УдостоверяющийЦентр'):
         data = data_certs.find('Данные')
         date = data_certs.find('ПериодДействияС')
 
-        #if date.text[0:4] == "2020" or date.text[0:4] == "2019" or date.text[0:4] == "2021":
-        new_name = name.text.replace('"', '«')
-        new_name = new_name.replace('"', '»')
-        file_cert = open('C:/Users/a.karpihin/Desktop/certs/рабочие сертификаты/' + new_name + ".crt", "w+")
-        file_cert.write(data.text + '\n')
-        file_cert.close()
         back_address = key.find('АдресаСписковОтзыва')
         url_link = back_address.find('Адрес')
-        try:
-            urlreq.urlretrieve(url_link.text, 'C:/Users/a.karpihin/Desktop/certs/отозванные/' + new_name + '.crl')
-        except:
-            #logging.error(f'Error for link {url_link.text}')
-            logging.exception(f'Error for link {url_link.text}')
-            continue
+
+        if date.text[0:4] == "2020" or date.text[0:4] == "2019" or date.text[0:4] == "2021":
+            new_name = name.text.replace('"', '«')
+            new_name = new_name.replace('"', '»')
+            file_cert = open('C:/Users/a.karpihin/Desktop/certs/рабочие сертификаты/' + new_name + ".crt", "w+")
+            file_cert.write(data.text + '\n')
+            file_cert.close()
+            logging.info(f'Page {url_link.text} was SELECTED because it SATISFIES condition 2019|2020|2021')
+            try:
+                urlreq.urlretrieve(url_link.text, 'C:/Users/a.karpihin/Desktop/certs/отозванные/' + new_name + '.crl')
+            except:
+                logging.exception(f'Error for link {url_link.text}')
+                continue
+        else:
+            logging.info(f'Page {url_link.text} was NOT SELECTED because it does NOT SATISFIES condition 2019|2020|2021')
 
     i = i + 1
 
